148eefb0831057ebcd2da25ab917c429888d5712
Bbbgl <104217088+Bbbgl@users.noreply.github.com>
2022-07-02 11:16:05 +0200

    Update clickers.js

--- ARSupport-second/clickers.js
+++ ARSupport-second/clickers.js
@@ -9,14 +9,14 @@
                 // ci metterò un innerHTML per un bottone 
                 document.getElementById("myInstruction").innerHTML = "Insert that disk as shown";
                 element.setAttribute("gltf-model","https://raw.githubusercontent.com/Bbbgl/ARsupport.github.io/main/ARsupport/assets/devices/disk1/plate_red.glb")
-                element.setAttribute("scale","3 3 3")
+                element.setAttribute("scale","243 243 243")
             }
 
             if(code_device=="B"){
                             // ci metterò un innerHTML per un bottone 
                              document.getElementById("myInstruction").innerHTML = "Insert that disk as shown";
                              element.setAttribute("gltf-model","https://raw.githubusercontent.com/Bbbgl/ARsupport.github.io/main/ARsupport/assets/devices/disk1/plate_yellow.glb")
-                            element.setAttribute("scale","3 3 3")
+                            element.setAttribute("scale","243 243 243")
                      }
 
             
@@ -27,30 +27,3 @@
 
 
 
-
-
-// AFRAME.registerComponent('clicker', {
-//     init: function () {
-
-//         //const el = document.getElementById("marker1");        
-//         this.el.addEventListener('click', e => {
-//             var element = document.querySelector('#disk')
-
-//             if(code_device=="A"){
-//                 // ci metterò un innerHTML per un bottone 
-//                 document.getElementById("myInstruction").innerHTML = "Insert that disk as shown";
-//                 element.setAttribute("gltf-model","https://raw.githubusercontent.com/Bbbgl/ARsupport.github.io/main/ARsupport/assets/devices/disk1/plate_red.glb")
-//                 element.setAttribute("scale","3 3 3")
-//         }
-
-//         if(code_device=="B"){
-//             // ci metterò un innerHTML per un bottone 
-//             document.getElementById("myInstruction").innerHTML = "Insert that disk as shown";
-//             element.setAttribute("gltf-model","https://raw.githubusercontent.com/Bbbgl/ARsupport.github.io/main/ARsupport/assets/devices/disk1/blue_roll.glb")
-//             element.setAttribute("scale","3 3 3")
-//     }
-            
-            
-//         })
-//     }
-// }); 
