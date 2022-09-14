66799b8290d715ba40f3cef58cc83234d01409bd
Bbbgl <104217088+Bbbgl@users.noreply.github.com>
2022-09-14 17:46:53 +0200

    Update clickers.js

--- ARSupport-engine/clickers.js
+++ ARSupport-engine/clickers.js
@@ -12,33 +12,33 @@
         btn1.addEventListener("click", e => {
                             
             element.setAttribute("gltf-model","https://raw.githubusercontent.com/Bbbgl/ARsupport.github.io/main/ARsupport/assets/devices/animated_engine/crankshaft.glb")     
-            element.setAttribute("scale","5 5 5") 
+            element.setAttribute("scale","50 50 50") 
         })
 
         btn2.addEventListener("click", e => {
                             
             element.setAttribute("gltf-model","https://raw.githubusercontent.com/Bbbgl/ARsupport.github.io/main/ARsupport/assets/devices/animated_engine/piston.glb")   
-            element.setAttribute("scale","0.3 0.3 0.3")    
+            element.setAttribute("scale","3 3 3")    
         })
         btn3.addEventListener("click", e => {
                             
             element.setAttribute("gltf-model","https://raw.githubusercontent.com/Bbbgl/ARsupport.github.io/main/ARsupport/assets/devices/animated_engine/chain.glb") 
-            element.setAttribute("scale","0.3 0.3 0.3")      
+            element.setAttribute("scale","3 3 3")      
         })
         btn4.addEventListener("click", e => {
                             
             element.setAttribute("gltf-model","https://raw.githubusercontent.com/Bbbgl/ARsupport.github.io/main/ARsupport/assets/devices/animated_engine/throttleBody.glb")
-            element.setAttribute("scale","0.1 0.1 0.1")       
+            element.setAttribute("scale","1 1 1")       
         })
         btn5.addEventListener("click", e => {
                             
             element.setAttribute("gltf-model","https://raw.githubusercontent.com/Bbbgl/ARsupport.github.io/main/ARsupport/assets/devices/animated_engine/cylinders.glb") 
-            element.setAttribute("scale","0.1 0.1 0.1")      
+            element.setAttribute("scale","1 1 1")      
         })
         btn6.addEventListener("click", e => {
                             
             element.setAttribute("gltf-model","https://raw.githubusercontent.com/Bbbgl/ARsupport.github.io/main/ARsupport/assets/devices/animated_engine/scene.gltf") 
-            element.setAttribute("scale","0.5 0.5 0.5")      
+            element.setAttribute("scale","5 5 5")      
         })
     }
 })
